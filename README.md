e6977d79dea89dd372c10c8f160181fc8249d977
Ravi Chuahan <ravichauhan0407@gmail.com>
2022-09-11 22:59:45 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# find-your-cocktail
+you can search for any cocktail and see steps how  to make a drink and ingredients of cocktail
+# DEMO 
+https://priceless-noether-fe413a.netlify.app/
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Welcome to our cocktail DB. 
+      </p>
+    </section>
+  );
+}
